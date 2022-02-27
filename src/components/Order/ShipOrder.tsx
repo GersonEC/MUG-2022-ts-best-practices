b57ddbf68fb07ec63c6dc70c59d384ae1ed8468a
Luca Del Puppo <luca@delpuppo.net>
2022-02-27 18:54:44 +0100

    add zod

--- src/components/Order/ShipOrder.tsx
+++ src/components/Order/ShipOrder.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement } from 'react';
-import type { ShipOrderModel } from '../../core';
-import { currency, date } from '../../core/utils';
+import { currency, date, ShipOrderModel } from '../../core';
 
 type ShipOrderProps = {
     order: ShipOrderModel,
@@ -23,9 +22,9 @@ export default function ShipOrder({ order, onView }: ShipOrderProps): ReactEleme
             <div className="content has-text-left">
                 <em>Total: </em><strong>{currency.format(order.total)}</strong>
                 <br />
-                <em>Order Date: </em><strong>{date.format(date.decode(order.date))}</strong>
+                <em>Order Date: </em><strong>{date.format(order.date)}</strong>
                 <p>
-                    <em>Shipped in date </em><strong>{date.format(date.decode(order.ship.date))}</strong>
+                    <em>Shipped in date </em><strong>{date.format(order.ship.date)}</strong>
                     <br />
                     <em>With: </em><strong>{order.ship.company}</strong>
                     <br />
